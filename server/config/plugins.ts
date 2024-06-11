e1f5a1b5754019adcbdf72f117bf99a53bf8d402
obranen <krakhnytskyi@gmail.com>
2024-06-11 14:46:07 +0300

    update

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -1,10 +1,21 @@
 export default ({ env }) => ({
+  upload: {
+    config: {
+      breakpoints: {
+        "3xl": 1686,
+        "2xl": 1400,
+        xl: 1200,
+        lg: 992,
+        md: 768,
+        sm: 576,
+        mobile: 320,
+      },
+    },
+  },
   "react-icons": true,
   "entity-relationship-chart": {
     enabled: true,
     config: {
-      // By default all contentTypes and components are included.
-      // To exlclude strapi's internal models, use:
       exclude: [
         "strapi::core-store",
         "webhook",
