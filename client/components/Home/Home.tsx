d34418fff0a4e3a41c7e40401251cc10a20dcfaf
Obranen <hgejepq419AJCMsla12d>
2024-07-02 19:08:46 +0300

    update

--- client/components/Home/Home.tsx
+++ client/components/Home/Home.tsx
@@ -1,14 +1,14 @@
-import WaveSVG from '@/public/images/svg/WaveSVG'
-import FindSite from '../header/FindSite'
-import AboutUs from './aboutUs/AboutUs'
-import MainImg from './MainImg'
-import OurProduction from './ourProduction/OurProduction'
-import SliderPromotion from './sliderPromotion/SliderPromotion'
+// import WaveSVG from '@/public/images/svg/WaveSVG'
+// import FindSite from '../header/FindSite'
+// import AboutUs from './aboutUs/AboutUs'
+// import MainImg from './MainImg'
+// import OurProduction from './ourProduction/OurProduction'
+// import SliderPromotion from './sliderPromotion/SliderPromotion'
 
 const Home = () => {
   return (
     <div className='lg:bg-primaryLight'>
-      <div className='relative lg:h-[459px] lg:-mt-10 xl:h-[555px] lg:bg-secondaryLight 2xl:h-[650px] 2xl:mt-[-30px] 3xl:h-[886px] 3xl:mt-[-93px]'>
+      {/* <div className='relative lg:h-[459px] lg:-mt-10 xl:h-[555px] lg:bg-secondaryLight 2xl:h-[650px] 2xl:mt-[-30px] 3xl:h-[886px] 3xl:mt-[-93px]'>
         <div className='container'>
           <SliderPromotion />
           <div className='md:mt-6 lg:hidden'>
@@ -18,7 +18,7 @@ const Home = () => {
         <MainImg />
       </div>
       <WaveSVG className='w-full xl:-m-0.5' fill='#f6eee8' />
-      <OurProduction />
+      <OurProduction /> */}
       {/* <AboutUs /> */}
     </div>
   )
