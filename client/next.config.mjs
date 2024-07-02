0b9168f8dc0df40a18cfc88d6881a089202147c6
Obranen <hgejepq419AJCMsla12d>
2024-07-02 18:52:02 +0300

    update

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -6,16 +6,16 @@ import withPlaiceholder from '@plaiceholder/next'
 const config = {
   images: {
     remotePatterns: [
-      // {
-      //   protocol: 'http',
-      //   hostname: 'localhost',
-      //   port: '1337',
-      // },
       {
-        protocol: 'https',
-        hostname: 'bakery-server-w2iq.onrender.com',
+        protocol: 'http',
+        hostname: 'localhost',
         port: '1337',
       },
+      // {
+      //   protocol: 'https',
+      //   hostname: 'bakery-server-w2iq.onrender.com',
+      //   port: '1337',
+      // },
     ],
     deviceSizes: [320, 576, 768, 992, 1200, 1400, 1686, 1920],
   },
