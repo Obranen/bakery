8ad1c5616effe8b14a8f9a7e8c1cb092401130c1
Obranen <hgejepq419AJCMsla12d>
2024-07-02 20:32:13 +0300

    update

--- client/data/navigation.data.ts
+++ client/data/navigation.data.ts
@@ -1,57 +1,57 @@
-import { INavigation } from '@/interface/navigation.interface'
+// import { INavigationState } from '@/interface/navigation.interface'
 
-export const navigation: INavigation[] = [
-  {
-    id: '1',
-    href: '#',
-    title: 'Увійти',
-    isDropdown: false,
-    isModal: true,
-    modalId: 'my_modal_1',
-  },
-  {
-    id: '2',
-    href: '',
-    title: 'Продукція',
-    isDropdown: true,
-    dropdown: [
-      {
-        id: '1',
-        href: 'baking',
-        title: 'Випічка',
-      },
-      {
-        id: '2',
-        href: 'bread',
-        title: 'Хліб',
-      },
-      {
-        id: '3',
-        href: 'dessert',
-        title: 'Десерт',
-      },
-    ],
-    isModal: false,
-  },
-  {
-    id: '3',
-    href: '/contact',
-    title: 'Контакти',
-    isDropdown: false,
-    isModal: false,
-  },
-  {
-    id: '4',
-    href: '/promotion',
-    title: 'Акції',
-    isDropdown: false,
-    isModal: false,
-  },
-  {
-    id: '5',
-    href: '#',
-    title: 'Кошик',
-    isDropdown: false,
-    isModal: false,
-  },
-]
+// export const navigation: INavigationState[] = [
+//   {
+//     id: '1',
+//     href: '#',
+//     title: 'Увійти',
+//     isDropdown: false,
+//     isModal: true,
+//     modalId: 'my_modal_1',
+//   },
+//   {
+//     id: '2',
+//     href: '',
+//     title: 'Продукція',
+//     isDropdown: true,
+//     dropdown: [
+//       {
+//         id: '1',
+//         href: 'baking',
+//         title: 'Випічка',
+//       },
+//       {
+//         id: '2',
+//         href: 'bread',
+//         title: 'Хліб',
+//       },
+//       {
+//         id: '3',
+//         href: 'dessert',
+//         title: 'Десерт',
+//       },
+//     ],
+//     isModal: false,
+//   },
+//   {
+//     id: '3',
+//     href: '/contact',
+//     title: 'Контакти',
+//     isDropdown: false,
+//     isModal: false,
+//   },
+//   {
+//     id: '4',
+//     href: '/promotion',
+//     title: 'Акції',
+//     isDropdown: false,
+//     isModal: false,
+//   },
+//   {
+//     id: '5',
+//     href: '#',
+//     title: 'Кошик',
+//     isDropdown: false,
+//     isModal: false,
+//   },
+// ]
--- client/data/sliderPromotion.data.ts
+++ client/data/sliderPromotion.data.ts
@@ -1,24 +1,24 @@
-import { ISliderPromotion } from '@/interface/sliderPromotion.interface'
+// import { ISliderPromotion } from '@/interface/sliderPromotion.interface'
 
-export const sliderPromotion: ISliderPromotion[] = [
-  {
-    id: '1',
-    src: '/images/slide-main-1.webp',
-    alt: 'Скидки по вечерам 80%.',
-  },
-  {
-    id: '2',
-    src: '/images/slide-main-1.webp',
-    alt: 'Каждый третий товар бесплатно.',
-  },
-  {
-    id: '3',
-    src: '/images/slide-main-1.webp',
-    alt: 'Акция 3.',
-  },
-  {
-    id: '4',
-    src: '/images/slide-main-1.webp',
-    alt: 'Акция 4.',
-  },
-]
+// export const sliderPromotion: ISliderPromotion[] = [
+//   {
+//     id: '1',
+//     src: '/images/slide-main-1.webp',
+//     alt: 'Скидки по вечерам 80%.',
+//   },
+//   {
+//     id: '2',
+//     src: '/images/slide-main-1.webp',
+//     alt: 'Каждый третий товар бесплатно.',
+//   },
+//   {
+//     id: '3',
+//     src: '/images/slide-main-1.webp',
+//     alt: 'Акция 3.',
+//   },
+//   {
+//     id: '4',
+//     src: '/images/slide-main-1.webp',
+//     alt: 'Акция 4.',
+//   },
+// ]
